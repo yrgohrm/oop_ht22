30d190c32ba5f9aabcdddd73378b419e99d4015d
Hampus Ram <hampus.ram@educ.goteborg.se>
2022-09-09 15:19:08 +0200

    annat namn och text

---
+++ lektion2/FunnyString.java
@@ -0,0 +1,8 @@
+public class FunnyString {
+    public static void main(String[] args) {
+        int myVar = 5;
+        System.out.println("The value is: " + myVar);
+        System.out.println("The value is: " + myVar + 2);
+        System.out.println("The value is: " + (myVar + 2));
+    }
+}
