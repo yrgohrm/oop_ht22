f12e59f85e5d342a16ec64171df6f51c83857b7d
Hampus Ram <hampus.ram@educ.goteborg.se>
2022-09-09 14:23:45 +0200

    lektion 2

---
+++ lektion2/App.java
@@ -0,0 +1,9 @@
+public class App {
+    public static void main(String[] args) {
+        byte a = 31;
+        byte b = 120;
+        int d = a + b;
+        byte c = (byte)d;
+        System.out.println(c);
+    }
+}
